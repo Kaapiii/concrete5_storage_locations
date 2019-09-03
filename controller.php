7f196d3a802df1a82a228937f11ff0b07b60e21c
Markus Liechti <kaapiii@hotmail.com>
2019-09-04 00:37:57 +0200

    update the dependencies an the package controller for google cloud storage location

--- controller.php
+++ controller.php
@@ -3,7 +3,10 @@
 namespace Concrete\Package\Concrete5StorageLocations;
 
 use Concrete\Core\Package\Package;
+use Concrete\Core\Entity\Package as PackageEntity;
 use Concrete\Core\File\StorageLocation\Type\Type as StorageLocationType;
+use Concrete\Core\Package\PackageService;
+use Concrete\Core\Support\Facade\Application as ApplicationFacade;
 
 /**
  * Package Controller - Concrete5 Storage Locations
@@ -12,13 +15,13 @@ class Controller extends Package
 {
     protected $pkgHandle = 'concrete5_storage_locations';
     protected $appVersionRequired = '8.0.0';
-    protected $pkgVersion = '0.0.1';
+    protected $pkgVersion = '0.2';
 
     protected $pkgAutoloaderMapCoreExtensions = true;
 
     public function getPackageDescription()
     {
-        return t('Packages add new storage locations for files on AWS S3 & Google Cloud Storage');
+        return t('Package adds new storage locations on AWS S3 & Google Cloud Storage');
     }
 
     public function getPackageName()
@@ -30,7 +33,50 @@ class Controller extends Package
     {
         $pkg = parent::install();
 
-        StorageLocationType::add('aws_s3', 'Amazon S3 (Kaapiii)', $pkg);
-        //StorageLocationType::add('google_cloud_storage', 'Google Cloud Storage', $pkg);
+        $this->installStorageLocations($pkg);
+    }
+
+    public function upgrade()
+    {
+        parent::upgrade();
+
+        $app = ApplicationFacade::getFacadeApplication();
+        $pkg = $app->make(PackageService::class)->getByHandle($this->pkgHandle);
+
+        $this->installStorageLocations($pkg);
+    }
+
+    /**
+     * Install supported storage locations
+     *
+     * @param PackageEntity $pkg
+     */
+    protected function installStorageLocations(PackageEntity $pkg)
+    {
+        $storageLocations = $this->getSupportedStorageLocations();
+
+        if(is_array($storageLocations) && count($storageLocations) > 0){
+            foreach($storageLocations as $key => $label){
+
+                $strglctn = StorageLocationType::getByHandle($key);
+
+                if(!is_object($strglctn)){
+                    StorageLocationType::add($key, $label, $pkg);
+                }
+            }
+        }
+    }
+
+    /**
+     * Array with supported storage locations
+     *
+     * @return array
+     */
+    protected function getSupportedStorageLocations()
+    {
+        return [
+            'aws_s3' => 'Amazon S3',
+            'google_cloud_storage' => 'Google Cloud Storage'
+        ];
     }
 }
