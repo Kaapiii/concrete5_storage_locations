9f8b26993309992be21a9e45c68717dd1235c2b8
Markus Liechti <kaapiii@hotmail.com>
2019-09-04 22:27:47 +0200

    update to version 0.3

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'concrete5_storage_locations';
     protected $appVersionRequired = '8.0.0';
-    protected $pkgVersion = '0.2';
+    protected $pkgVersion = '0.3';
 
     protected $pkgAutoloaderMapCoreExtensions = true;
 
